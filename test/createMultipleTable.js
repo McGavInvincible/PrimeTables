fc44da0ca6dcf6bfc584d759cc8f3b918ba33773
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 22:37:01 +0100

    Fixed first test

--- test/createMultipleTable.js
+++ test/createMultipleTable.js
@@ -4,7 +4,7 @@ var multipleTable = require("../source/createMultipleTable.js");
 describe("Multiple Table Generator", function() {
     it("Generates a 2d array with row and column headers and a table within", function() {
 
-	var tabkeOne = arrayMultiply.arrayMultiply([1, 2, 3],[2, 3, 4],[[2, 3, 4], [4, 6, 8], [6, 9, 12]]);
+	var tableOne = multipleTable.createMultipleTable([1, 2, 3],[2, 3, 4],[[2, 3, 4], [4, 6, 8], [6, 9, 12]]);
 
 	expect(tableOne).to.deep.equal([[[], 1, 2, 3],[2, 2, 3, 4], [3, 4, 6, 8], [4, 6, 9, 12]]);
 		    

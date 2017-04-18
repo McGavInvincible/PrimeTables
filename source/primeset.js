2630205d5af08104dfbeb8fd47eebb5e8277b996
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 15:12:50 +0100

    Updated brute prime method and test
    setPrimeBrute passed first test, test now updated for non-whole numbers

--- source/primeset.js
+++ source/primeset.js
@@ -4,6 +4,46 @@ exports.primeSetBrute = function(n) {
 
     //Generates a set of prime numbers of size, n, by comparing to all previous primes
 
-    return false;
+    var prime = [],
+	i = 0,
+	j = 0,
+	isPrime = false;
+
+    //Begin method with first prime, 2, and first number to check, 3
+    prime[0] = 2;
+    i = 3;
+
+    //Do until the array prime is of size n 
+    while (prime.length < n) {
+
+	isPrime = true;
+
+	//Compare possible prime, i, to all currently found primes
+	for (j = 0; j < prime.length; j++) {
+
+	    if (i % prime[j] === 0) {
+
+		isPrime = false;
+		break;
+
+	    } else if (Math.pow(prime[j],2) > i) {
+		//Stop checking if current prime squared is larger than i
+		break;
+
+	    }
+	}
+
+	//If found to be prime, push onto array of found primes
+	if (isPrime === true) {
+
+	    prime.push(i);
+
+	}
+
+	i++
+
+    }
+
+    return prime;
 
 };
--- test/primeset.js
+++ test/primeset.js
@@ -7,9 +7,15 @@ describe("Prime Set Generator", function() {
 
 	    var oneP = primeSet.primeSetBrute(1);
 	    var tenP = primeSet.primeSetBrute(10);
+	    var zeroP = primeSet.primeSetBrute(0);
+	    var nonintegerP = primeSet.primeSetBrute(1.5);
+	    var wordP = primeSet.primeSetBrute("Primes please");
 	
 	    expect(oneP).to.deep.equal([2]);
 	    expect(tenP).to.deep.equal([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+	    expect(zeroP).to.deep.equal(0);
+	    expect(nonintegerP).to.deep.equal(0);
+	    expect(wordP).to.deep.equal(0);
 
 	});
     });

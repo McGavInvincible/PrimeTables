01e0b465132d5bc2ace95a121275916d492eefcc
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 16:11:30 +0100

    Integer generator passed first test

--- source/integerset.js
+++ source/integerset.js
@@ -1,7 +1,16 @@
 exports.integersFromTo = function(startn, endn) {
 
     //Returns a set of all integers from startn to endn of size (endn-startn)+1
+
+    var intArray = [],
+	intCounter = 0;
     
-    return false;
+    intArray[0] = startn;
+    
+    for (intCounter = startn+1; intCounter <= endn; intCounter++) {
+	intArray.push(intCounter);
+    }
+
+    return intArray;
 
 }

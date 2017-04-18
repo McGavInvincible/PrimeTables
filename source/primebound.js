4ffe40d4b734beaf4ed4c612a20c3c17d053dff7
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 16:55:52 +0100

    Prime upper bound passes new test

--- source/primebound.js
+++ source/primebound.js
@@ -2,13 +2,31 @@ exports.primeUpperBound = function(n) {
 
     //Returns upper bound for value of nth prime
 
-    var primeUpper = [];
+    var primeUpper = 0;
 
-    primeUpper = n * (Math.log(n) + Math.log(Math.log(n)));
+    switch (n) {
+	
+    case 1:
+	primeUpper = 2;
+	break;
 
-    //Setting bound to a whole number
-    primeUpper = Math.ceil(primeUpper);
+    case 2:
+	primeUpper = 3;
+	break;
 
+    case 3:
+	primeUpper = 5;
+	break;
+
+    default:
+	
+	primeUpper = n * (Math.log(n) + Math.log(Math.log(n)));
+
+	//Setting bound to a whole number
+	primeUpper = Math.ceil(primeUpper);
+
+    }
+    
     return primeUpper;
     
 };

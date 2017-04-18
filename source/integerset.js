3fe66e08539a62d91bf7bf19378f6e4f9326267f
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 16:25:46 +0100

    Integerset now passes test

--- source/integerset.js
+++ source/integerset.js
@@ -2,9 +2,27 @@ exports.integersFromTo = function(startn, endn) {
 
     //Returns a set of all integers from startn to endn of size (endn-startn)+1
 
+    //Checks parameters are both integers
+    if (typeof startn !== "number" || (startn%1) !== 0) {
+	console.log("Please enter an integer");
+	return 0;
+    }
+
+    if (typeof endn !== "number" || (endn%1) !== 0) {
+	console.log("Please enter an integer");
+	return 0;
+    }
+
+    //Checks parameters are in correct order
+    if (startn > endn) {
+	console.log("Starting number cannot be greater than ending number");
+	return 0;
+    }
+    
     var intArray = [],
 	intCounter = 0;
-    
+
+    //Pushes next integer onto array until next integer is greater than endn
     intArray[0] = startn;
     
     for (intCounter = startn+1; intCounter <= endn; intCounter++) {

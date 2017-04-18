b247aae3a57e5e6e74d51024ecf5eedfe1fe9654
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 19:01:58 +0100

    Array multiplier passes test

--- source/arraymultiply.js
+++ source/arraymultiply.js
@@ -1,6 +1,23 @@
 exports.arrayMultiply = function(m, n) {
 
     //Returns a 2-d array of the multiples of the elements of m and n
-    return false;
+    var multiplyMatrix = [],
+	temporaryMatrix = [],
+	mcounter = 0,
+	ncounter = 0;
+    
+    for (var mCounter = 0; mCounter < m.length; mCounter++) {
+
+	temporaryMatrix = n.slice();
+
+	for (var nCounter = 0; nCounter < temporaryMatrix.length; nCounter++) {
+	    temporaryMatrix[nCounter] *= m[mCounter]
+	}
+
+	multiplyMatrix.push(temporaryMatrix);
+
+    }
+
+    return multiplyMatrix;
 
 };

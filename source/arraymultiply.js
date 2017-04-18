0fc2794e23cf9c1c5209acca1b061526dc2905fd
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 18:48:57 +0100

    Initial test and failing method for array multiplier

---
+++ source/arraymultiply.js
@@ -0,0 +1,6 @@
+exports.arrayMultiply = function(m, n) {
+
+    //Returns a 2-d array of the multiples of the elements of m and n
+    return false;
+
+};
---
+++ test/arraymultiply.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect;
+var arrayMultiply = require("../source/arraymultiply.js");
+
+describe("Multiple Table Generator", function() {
+    it("Generates a 2d array of the multiples of 2 given arrays", function() {
+
+	var arrayOne = arrayMultiply.arrayMultiply([1, 2, 3],[2, 3, 4]);
+
+	expect(arrayOne).to.deep.equal([[2, 3, 4], [4, 6, 8], [6, 9, 12]);
+		    
+    });
+});
+

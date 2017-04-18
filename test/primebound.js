81c80851288f62038a9abc0b10c90a7b65f0c97e
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 16:50:24 +0100

    Updated primebound test for low values of n

--- test/primebound.js
+++ test/primebound.js
@@ -7,10 +7,20 @@ describe("Prime Bound", function() {
 	var upperTen = primeBound.primeUpperBound(10);
 	var upperHundred = primeBound.primeUpperBound(100);
 	var upperThousand = primeBound.primeUpperBound(1000);
-
+	var upperOne = primeBound.primeUpperBound(1);
+	var upperTwo = primeBound.primeUpperBound(2);
+	var upperThree = primeBound.primeUpperBound(3);
+	var upperFour = primeBound.primeUpperBound(4);
+	var upperFive = primeBound.primeUpperBound(5);
+	
 	expect(upperTen).to.equal(32);
 	expect(upperHundred).to.equal(614);
 	expect(upperThousand).to.equal(8841);
+	expect(upperOne).to.equal(2);
+	expect(upperTwo).to.equal(3);
+	expect(upperThree).to.equal(5);
+	expect(upperFour).to.equal(7);
+	expect(upperFive).to.equal(11);
 	
     });
 });

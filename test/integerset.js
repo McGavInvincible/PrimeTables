3c0eae27fd7b7147d847429b6ad5ff1ecaa09f92
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-18 16:20:30 +0100

    Updated integertest for paramater checks

--- test/integerset.js
+++ test/integerset.js
@@ -7,10 +7,24 @@ describe("Integer Array Generator", function() {
 	var oneToTen = integerSet.integersFromTo(1,10);
 	var minusToPlus = integerSet.integersFromTo(-2,2);
 	var singleDigit = integerSet.integersFromTo(5,5);
+	var wrongWay = integerSet.integersFromTo(10,1);
+	var nonIntegerFirst = integerSet.integersFromTo(0.5,1);
+	var nonIntegerSecond = integerSet.integersFromTo(1,1);
+	var nonIntegerBoth = integerSet.integersFromTo(0.5,1.5);
+	var wordFirst = integerSet.integersFromTo("Start",1);
+	var wordSecond = integerSet.integersFromTo(1,"End");
+	var wordBoth = integerSet.integersFromTo("Start","End");
 
 	expect(oneToTen).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 	expect(minusToPlus).to.deep.equal([-2, -1, 0, 1, 2]);
 	expect(singleDigit).to.deep.equal([5]);
+	expect(wrongWay).to.deep.equal(0);
+	expect(nonIntegerFirst).to.deep.equal(0);
+	expect(nonIntegerSecond).to.deep.equal(0);
+	expect(nonIntegerBoth).to.deep.equal(0);
+	expect(wordFirst).to.deep.equal(0);
+	expect(wordSecond).to.deep.equal(0);
+	expect(wordBoth).to.deep.equal(0);
 	    
     });
 });

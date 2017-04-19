7513e37565c6cf854424779ca13f86d5236af102
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-19 15:19:04 +0100

    Test and failing method for finding max element of 2d array

---
+++ source/maxtwodimarray.js
@@ -0,0 +1,7 @@
+exports.maxArray = function(n) {
+
+    //Returns the maximum element of a 2-d array
+
+    return false;
+    
+};
---
+++ test/maxtwodimarray.js
@@ -0,0 +1,15 @@
+var expect = require("chai").expect;
+var maxTwoDimArray = require("../source/maxtwodimarray.js");
+
+describe("Maximum Element Finder", function() {
+    it("Finds the maximum element of a 2-d array", function() {
+
+	var allnumbers = maxTwoDimArray.maxArray([[1, 2, 3],[2, 3, 4]]);
+	var somenumbers = maxTwoDimArray.maxArray([[1, 2, "Word"],[2, [], 4]]);
+
+	expect(allnumbers).to.equal(4);
+	expect(somenumbers).to.equal(4);
+		    
+    });
+});
+

96b6eb8f1241e189f75a90f1cbfd1cc9c30ffc07
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-19 15:06:54 +0100

    Table evenly spaced assuming last element of last row is the longest

--- source/primetabledisplay.js
+++ source/primetabledisplay.js
@@ -4,7 +4,20 @@ exports.primeTableDisplay = function(m) {
     
     var outString = "",
 	rowCounter = 0,
-	colCounter = 0;
+	colCounter = 0,
+	spaecCounter = 0,
+	maxEntry = 0,
+	maxLength = 0,
+	minTableSpace = 0;
+
+    //Smallest spacing for elements of array
+    minTableSpace = 4;
+
+    //Assumes longest element is the last element of last row
+    maxEntry = m[m.length-1][m[m.length-1].length-1];
+    maxLength = maxEntry.toString().length;
+
+    if (maxLength < minTableSpace) {maxLength = minTableSpace}
     
     for (rowCounter = 0; rowCounter < m.length; rowCounter++) {
 	
@@ -12,6 +25,11 @@ exports.primeTableDisplay = function(m) {
 	
 	for (colCounter = 0; colCounter < m[rowCounter].length; colCounter++) {
 
+	    //Fills in empty space such that all elements of table are equally spaced
+	    for (spaceCounter = 0; spaceCounter < maxLength-m[i][j].toString().length; spaceCounter++) {
+		outString = outString.concat(" ");
+	    }
+	    
 	    outString = outString.concat(" ",m[rowCounter][colCounter], " |")
 	    
 	}

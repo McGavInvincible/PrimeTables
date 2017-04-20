a2ee980fd9973a44f3f3e58678734afc5b1de4bc
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-20 14:54:50 +0100

    Method passes

--- source/primetablegenerator.js
+++ source/primetablegenerator.js
@@ -2,6 +2,21 @@ exports.primeTableGenerator = function(n) {
 
     //Returns a table of prime multiples with the prime numbers as row and column headers
     
-    return false;
+    var primeSet = require("../source/primeset.js"),
+	arrayMultiply = require("../source/arraymultiply.js"),
+	multipleTable = require("../source/createmultipletable.js"),
+	primeTableDisplay = require("../source/primetabledisplay.js");
+
+    if (n < 100000) {
+	primes = primeSet.primeSetBrute(n);
+    } else {
+	primes = primeSet.primeSetSieve(n);
+    }
+
+    multiTable = arrayMultiply.arrayMultiply(primes, primes);
+
+    outputArray = multipleTable.createMultipleTable(primes, primes, multiTable);
+
+    return outputArray;
     
 }

acef45423086ff556f4de57489956db63b88a3d2
McGavInvincible <pdsmcgavin@gmail.com>
2017-04-20 15:00:49 +0100

    Updated main method with new primetablegenerator

--- source/primetable.js
+++ source/primetable.js
@@ -1,16 +1,12 @@
 /*Takes in a whole number, n, from the user, generates a set of primes of size n and then displays a table
   of the prime sets as column and row headers and their multiples as the table elements*/
 
-var primeSet = require("../source/primeset.js"),
-    arrayMultiply = require("../source/arraymultiply.js"),
-    multipleTable = require("../source/createmultipletable.js"),
-    primeTableDisplay = require("../source/primetabledisplay.js"),
+var primeTableDisplay = require("../source/primetabledisplay.js"),
     wholeNumber = require("../source/wholenumber.js"),
-    inquirer = require("inquirer");
+    inquirer = require("inquirer"),
+    primeTableGenerator = require("../source/primetablegenerator.js");
 
 var userNumber = 0,
-    primes = [],
-    multiTable = [],
     outputArray = [];
 
 var question = {
@@ -26,28 +22,13 @@ function main() {
 	userNumber = parseInt(answers.wholeN);
 
 	if (wholeNumber.isWholeNumber(userNumber) === true) {
-	    primeTable(userNumber)
+	    outputArray = primeTableGenerator.primeTableGenerator(userNumber);
+	    primeTableDisplay.primeTableDisplay(outputArray);
 	} else {
 	    main()
 	}
 	
     })
-}
-
-function primeTable(n) {
-
-    if (n < 100000) {
-	primes = primeSet.primeSetBrute(n);
-    } else {
-	primes = primeSet.primeSetSieve(n);
-    }
-
-    multiTable = arrayMultiply.arrayMultiply(primes, primes);
-
-    outputArray = multipleTable.createMultipleTable(primes, primes, multiTable);
-
-    primeTableDisplay.primeTableDisplay(outputArray);
-
 }
 
 main()
